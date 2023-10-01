c960ef5569fe3f31fdb08d85ffe05664e77ec1de
Pushkar Patil <pushkar@Pushkars-MacBook-Pro.local>
2023-10-01 16:04:28 -0400

    first commit

---
+++ backend/server.js
@@ -0,0 +1,8 @@
+const express=require('express');
+const bodyParser=require('body-parser');
+
+const app=express();
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(bodyParser.json());
+app.use(cors());
