9daaf8f7c2d8b945566a0b42708e8ba26da1094b
Pushkar Patil <pushkar@Pushkars-MacBook-Pro.local>
2023-12-08 16:38:22 -0500

    change23

--- backend/server.js
+++ backend/server.js
@@ -36,7 +36,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: "https://travel-guide-app.vercel.app",
+    origin: "*",
   })
 );
 app.use(bodyParser.json());
